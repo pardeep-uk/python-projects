84f128c55a27d7af3d6ab533f47285297f8c735d
Pardeep Singh <pardeepsingh7243@gmail.com>
2024-05-29 05:40:54 -0400

    done

--- day-26/main.py
+++ day-26/main.py
@@ -1,5 +1,15 @@
-numbers=[1,1,2,3,5,8,13,21,34,55]
+# List comprehension
 
-squred_number = [num*num for num in numbers]
+# numbers=[1,1,2,3,5,8,13,21,34,55]
 
-print(squred_number)
+# squred_number = [num*num for num in numbers]
+
+# print(squred_number)
+
+# Dictionary comprehension
+
+sentence = input().split( )
+
+result = {word:len(word) for word in sentence}
+
+print(result)
